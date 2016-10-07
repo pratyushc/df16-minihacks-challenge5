7cac6a9a2e143123599f3ff0a4d529d5efd5c3f6
Pratyush Chalasani <code@pratyush.net>
2016-10-06 22:07:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.post('/list', function(req, res) {
+app.get('/list', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
         if (err) console.log(err);
